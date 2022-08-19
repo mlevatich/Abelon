1dc8e660b1dce732adc5b1b3d855f6934a2af444
mlevatich <maxwell.levatich@yale.edu>
2022-08-19 17:09:52 -0400

    east forest finalized, upgrade shrubify

--- utilities/shrubify.py
+++ utilities/shrubify.py
@@ -23,9 +23,12 @@ with open('abelon/data/maps/' + sys.argv[1] + '/map.txt', 'r') as f:
 LR = True
 for i in range(len(grid)):
     for j in range(len(grid[i])):
+        add = lambda s: print("~{} {} {} {}".format(s, j + 1, i + 1, 'L' if LR else 'R'))
         if grid[i][j] not in colliding:
-            if random() <= 0.1:
-                print("~grass2 {} {} {}".format(j + 1, i + 1, 'L' if LR else 'R'))
-            elif random() <= 0.05:
-                print("~grass {} {} {}".format(j + 1, i + 1, 'L' if LR else 'R'))
+            if   random() <= 0.100: add("grass2")
+            elif random() <= 0.050: add("grass")
+            elif random() <= 0.020: add("rock")
+            elif random() <= 0.018: add("shafe")
+            elif random() <= 0.018: add("forniese")
+            elif random() <= 0.008: add("colblossom")
             LR = not LR
